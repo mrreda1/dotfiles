90e566d0889ca86d3d0031b6266ed45319b4f647
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-09-07 13:58:51 +0300

    Get cases from first parameter or clipboard

--- Fish/.config/fish/functions/addcase.fish
+++ Fish/.config/fish/functions/addcase.fish
@@ -1,3 +1,7 @@
 function addcase
-    getclip | tee ../templates/default.in
+    if test (count $argv) -lt 1
+        getclip | tee ../templates/default.in
+    else
+        echo $argv[1] | tee ../templates/default.in
+    end
 end
